82feaae774acc2843482ea35cfaa44e6746a0e0a
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2019-10-19 03:59:06 -0500

    Add code to display/hide procedure components

--- site.js
+++ site.js
@@ -45,3 +45,77 @@ if (document.querySelector('#design-page')) {
   });
 
 } // End of design page
+
+// For the design page
+
+if (document.querySelector('#experiment-page')) {
+
+  // get component buttons
+
+  var agarBtn = html.querySelector('#agar-button');
+  var gelBtn = html.querySelector('#gel-button');
+  var petBtn = html.querySelector('#pet-button');
+  var gibsonBtn = html.querySelector('#gibson-button');
+  var dnaMiniBtn = html.querySelector('#dna-mini-button');
+  var dnaHDBtn = html.querySelector('#dna-hd-button');
+  var pcrBtn = html.querySelector('#pcr-button');
+  var pcrPurifyBtn = html.querySelector('#pcr-purify-button');
+  var proteinBtn = html.querySelector('#protein-purify-button');
+
+  // Click listener for three design components
+
+  agarBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#agar');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+  gelBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#gel');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+  petBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#pet');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+  gibsonBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#gibson');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+  dnaMiniBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#dna-mini');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+  dnaHDBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#dna-hd');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+  pcrBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#pcr');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+  pcrPurifyBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#pcr-purify');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+  proteinBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#protein-purify');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+} // End of design page
