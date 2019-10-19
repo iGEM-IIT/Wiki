bba69b29386ad3adc512212c38a6f5191468cf08
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2019-10-19 02:06:20 -0500

    Optimize JS to be reusable and page-specific

--- site.js
+++ site.js
@@ -1,35 +1,9 @@
 var html = document.querySelector('html');
 
-// For the design page
-
-var primerBtn = html.querySelector('#primer-button');
-var petaseBtn = html.querySelector('#petase-button');
-var assayBtn = html.querySelector('#assay-button');
-
-// Click listener for three design components
-
-primerBtn.addEventListener('click', function(e) {
-  let designContent = html.querySelector('#primer');
-  closeOthers();
-  designContent.classList.toggle('visible');
-});
-
-petaseBtn.addEventListener('click', function(e) {
-  let designContent = html.querySelector('#petase');
-  closeOthers();
-  designContent.classList.toggle('visible');
-});
-
-assayBtn.addEventListener('click', function(e) {
-  let designContent = html.querySelector('#assay');
-  closeOthers();
-  designContent.classList.toggle('visible');
-});
-
 // Removes displayed information before displaying the new one
 
-function closeOthers () {
-  var infoContainer = html.querySelector('#design-information');
+function closeOthers() {
+  var infoContainer = html.querySelector('.sections-container');
   var infoSections = infoContainer.getElementsByClassName('section-content');
   // console.log(infoSections);
   for (let i = 0; i < infoSections.length; i++) {
@@ -40,4 +14,34 @@ function closeOthers () {
   }
 }
 
-// End of design page JS
+// For the design page
+
+if (document.querySelector('#design-page')) {
+
+  // get component buttons
+
+  var primerBtn = html.querySelector('#primer-button');
+  var petaseBtn = html.querySelector('#petase-button');
+  var assayBtn = html.querySelector('#assay-button');
+
+  // Click listener for three design components
+
+  primerBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#primer');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+  petaseBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#petase');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+  assayBtn.addEventListener('click', function(e) {
+    let designContent = html.querySelector('#assay');
+    closeOthers();
+    designContent.classList.toggle('visible');
+  });
+
+} // End of design page
